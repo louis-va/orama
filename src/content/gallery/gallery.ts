f7860e1cf1f10493e3a6d3ec503a0fa6b6e3d1e0
Louis Van Aken <vanakenlouis@gmail.com>
2025-02-24 17:10:33 +0100

    Add image gallery

---
+++ src/content/gallery/gallery.ts
@@ -0,0 +1,50 @@
+export const gallery = [
+  {
+    src: "Jumpertz-LeFilm_OramaProd_JP_-049.jpg",
+    description: "Tournage Jumpertz"
+  },
+  {
+    src: "Jumpertz-LeFilm_OramaProd_JP_-057.jpg",
+    description: "Tournage Jumpertz"
+  },
+  {
+    src: "Jade&Naia_Coeur_OramaProd_-026.jpg",
+    description: "Tournage Jade & Naia - Coeur"
+  },
+  {
+    src: "Jade&Naia_Coeur_OramaProd_-071.jpg",
+    description: "Tournage Jade & Naia - Coeur"
+  },
+  {
+    src: "Jade&Naia_Coeur_OramaProd_-079.jpg",
+    description: "Tournage Jade & Naia - Coeur"
+  },
+  {
+    src: "Jade&Naia_Coeur_OramaProd_-102.jpg",
+    description: "Tournage Jade & Naia - Coeur"
+  },
+  {
+    src: "Jumpertz-LeFilm_OramaProd_JP_-049.jpg",
+    description: "Tournage Jumpertz"
+  },
+  {
+    src: "Jumpertz-LeFilm_OramaProd_JP_-057.jpg",
+    description: "Tournage Jumpertz"
+  },
+  {
+    src: "Jade&Naia_Coeur_OramaProd_-026.jpg",
+    description: "Tournage Jade & Naia - Coeur"
+  },
+  {
+    src: "Jade&Naia_Coeur_OramaProd_-071.jpg",
+    description: "Tournage Jade & Naia - Coeur"
+  },
+  {
+    src: "Jade&Naia_Coeur_OramaProd_-079.jpg",
+    description: "Tournage Jade & Naia - Coeur"
+  },
+  {
+    src: "Jade&Naia_Coeur_OramaProd_-102.jpg",
+    description: "Tournage Jade & Naia - Coeur"
+  },
+]
